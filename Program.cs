ab8cb6d718f6dc3c6a545e391dfa82ad39a02b48
JuanCasZa <juanjose-castro2@hotmail.com>
2025-08-27 19:07:20 -0500

    Cambios

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Personas
+{
+    public String nombre;
+    public String cedula;
+    public String telefono;
+    public String correo;
+    public int edad;
+}
+
+public class Entrenadores : Personas
+{
+    public List<Profesiones> titulos;
+    public decimal salario;
+    public int experiencia;
+}
+
+public class Profesiones {
+    public String titulo;
+}
